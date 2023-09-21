848208363b0c90758f80664a8252805e2d492bf4
vanthang03719 <vanthang03719@gmail.com>
2023-09-21 22:43:45 +0700

    name

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 function App() {
   return (
     <div className=" w-full items-center justify-center h-screen flex">
-     <h1>Hello World</h1>
-     <p>This is a our project</p>
-     <p>asdasdasdasdasdas</p>
-     <h1>asdasdasdasd</h1>
-     <h1>Nguyen Hoang Minh</h1>
+      <h1>Hello World</h1>
+      <p>This is a our project</p>
+      <p>asdasdasdasdasdas</p>
+      <h1>asdasdasdasd</h1>
+      <h1>Nguyen Hoang Minh</h1>
+      <h1> Thándsad</h1>
     </div>
   );
 }
