9b842fdabd8c7fed06972f8472f9d4c95cac190d
bao long <huynhngocblong.com>
2023-09-21 22:12:24 +0700

    baolong-commit-p

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className=" w-full items-center justify-center h-screen flex">
      <h1>Hello World</h1>
      <p>This is a our project</p>
+     <p></p>
     </div>
   );
 }
