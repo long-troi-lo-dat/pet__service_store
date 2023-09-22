6a404e6bdac353ecfac25c023e80002db7820b2a
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-09-22 17:13:31 +0700

    Start Working

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,7 @@
 function App() {
   return (
     <div className=" w-full items-center justify-center h-screen flex">
-      <h1>Hello World</h1>
-      <p>This is a our project</p>
-      <p>asdasdasdasdasdas</p>
-      <h1>asdasdasdasd</h1>
-      <h1>Nguyen Hoang Minh</h1>
-      <h1> Thándsad</h1>
-      <h3>long long long</h3>
-      <h3>Luan test</h3>
-      <h1> alooooooo </h1>
-      <h1> huy </h1>
-      <h2> Lại là DG HOUSE ! Ryderrrrrrrrrrrrrrrrrrrrr </h2>
-      <h2> Lại là DG HOUSE ! Ryderrrrrrrrrrrrrrrrrrrrr </h2>
-
+      <h1>Start Working </h1>
 
     </div>
   );
