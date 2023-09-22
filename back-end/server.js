a1779e921940b9358df16daf638c0f026ec22274
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-09-22 17:45:12 +0700

    setup backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const app = express();
-const port = 8000; // Chọn số cổng bạn muốn sử dụng (thay đổi nếu cần)
+const mysql = require("mysql");
+const cors = require("cors");
+const port = 8000;
+
+
+const app = express()
+app.use(cors())
+
+// Connect Database
+const db = mysql.createConnection({
+  host: "localhost",
+  user:"root",
+  password:"",
+  database:""
+})
 
 // Định nghĩa các route và xử lý logic ở đây
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Setup successfully from backend');
 });
 
 // Khởi động máy chủ
