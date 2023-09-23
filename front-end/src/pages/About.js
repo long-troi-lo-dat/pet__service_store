ed82472b72212afe4a95117e45cde262cfe71c5a
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-09-23 23:19:44 +0700

    fix bug footer

--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -1,11 +1,192 @@
-import React from 'react';
+import React from "react";
+import imageAbout from "../assets/images-about/image1.png";
 
 function About(props) {
-    return (
-        <div>
-            About
+  return (
+    <>
+      <div className=" ">
+        <div className="w-full relative">
+          <img
+            src={imageAbout}
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            alt="Image-banner"
+          />
+          <div className="absolute inset-0 flex items-center justify-center text-center">
+            <p className="text-white text-7xl font-bold">
+              Giới Thiệu Về DGHOUSE
+            </p>
+          </div>
         </div>
-    );
+
+        <div className="flex items-center justify-center ">
+          <div className="max-w-3xl p-8 text-center text-white">
+            {/* Thêm nội dung của bạn ở đây */}
+            <h2 className="text-4xl font-bold mb-4">Tiêu đề nội dung</h2>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+            <p className="text-xl text-black">
+              Nội dung của bạn ở đây. Bạn có thể thêm văn bản, hình ảnh, hoặc
+              bất kỳ nội dung nào bạn muốn.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
