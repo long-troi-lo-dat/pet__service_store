c968b6b23f6d38957f68eb071ae7617d43f2e25c
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-09-28 15:54:49 +0700

    config router in App

---
+++ front-end/src/routes/index.js
@@ -0,0 +1,32 @@
+import About from "../pages/About";
+import Blog from "../pages/Blog";
+import Booking from "../pages/Booking";
+import Cart from "../pages/cart";
+import Contact from "../pages/Contact";
+import Detail from "../pages/detail";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/register";
+import Service from "../pages/Service";
+import Shop from "../pages/Shop";
+
+
+
+
+const publicRoutes = [
+    {path:"/", component: Home},
+    {path:"/about", component: About },
+    {path:"/service", component: Service },
+    {path:"/contact", component:Contact },
+    {path:"/shop", component: Shop },
+    {path:"/blog", component: Blog },
+    {path:"/booking", component:Booking },
+    {path:"/login", component: Login},
+    {path:"/register", component: Register},
+    {path:"/detail", component:Detail },
+    {path:"/cart", component:Cart },
+];
+
+const privateRoutes = []
+
+export { publicRoutes, privateRoutes }
