904d7efa61c9423288da2d5d0bc363efdfa68aaa
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-10-01 17:37:31 +0700

    routes app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import GlobalState from "./Context";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
-import Booking from "./pages/Booking";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Service from "./pages/Service";
-import Shop from "./pages/Shop";
-import Login from "./pages/Login";
-import Register from "./pages/register";
-
+import { publicRoutes } from "./routes";
 
 const App = () => {
   return (
@@ -22,16 +13,12 @@ const App = () => {
           <Navbar />
           <main>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/About" element={<About />} />
-              <Route path="/Service" element={<Service />} />
-              <Route path="/Contact" element={<Contact />} />
-              <Route path="/Shop" element={<Shop />} />
-              <Route path="/Blog" element={<Blog />} />
-              <Route path="/Booking" element={<Booking />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/Register" element={<Register />} />
-
+              {publicRoutes.map((item, index) => {
+                const Page = item.component;
+                return (
+                  <Route key={index} path={item.path} element={<Page />} />
+                );
+              })}
             </Routes>
           </main>
           <Footer />
