858a0857d6ba59056a65ddba66b2014db917fd4e
Bao Long <hnbaolong.4work@gmail.com>
2024-05-20 23:48:36 +0700

    add dist folder

--- back-end/index.js
+++ back-end/index.js
@@ -7,10 +7,9 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const nodemailer = require("nodemailer");
 const readline = require('readline');
+var config = require('config');
 let router = express.Router();
-// let $ = require('jquery');
-// const request = require('request');
-// const moment = require('moment');
+
 dotenv.config();
 
 const port = process.env.PORT || 8000;
@@ -1137,74 +1136,74 @@ app.post('/dathang', (req, res) => {
   })
 })
 
-//payment
-router.post('/create_payment_url', function (req, res, next) {
-  var ipAddr = req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
+// //payment
+// router.post('/create_payment_url', function (req, res, next) {
+//   var ipAddr = req.headers['x-forwarded-for'] ||
+//     req.connection.remoteAddress ||
+//     req.socket.remoteAddress ||
+//     req.connection.socket.remoteAddress;
 
-  var config = require('config');
-  var dateFormat = require('dateformat');
+//   var config = require('config');
+//   var dateFormat = require('dateformat');
 
 
-  var tmnCode = config.get('RIRKXC7Z');
-  var secretKey = config.get('vnp_HashSecret');
-  var vnpUrl = config.get('https://sandbox.vnpayment.vn/paymentv2/vpcpay.html');
-  var returnUrl = config.get('vnp_ReturnUrl');
+//   var tmnCode = config.get('RIRKXC7Z');
+//   var secretKey = config.get('vnp_HashSecret');
+//   var vnpUrl = config.get('https://sandbox.vnpayment.vn/paymentv2/vpcpay.html');
+//   var returnUrl = config.get('vnp_ReturnUrl');
 
-  var date = new Date();
+//   var date = new Date();
 
-  var createDate = dateFormat(date, 'yyyymmddHHmmss');
-  var orderId = dateFormat(date, 'HHmmss');
-  var amount = req.body.amount;
-  var bankCode = req.body.bankCode;
+//   var createDate = dateFormat(date, 'yyyymmddHHmmss');
+//   var orderId = dateFormat(date, 'HHmmss');
+//   var amount = req.body.amount;
+//   var bankCode = req.body.bankCode;
 
-  var orderInfo = req.body.orderDescription;
-  var orderType = req.body.orderType;
-  var locale = req.body.language;
-  if (locale === null || locale === '') {
-    locale = 'vn';
-  }
-  var currCode = 'VND';
-  var vnp_Params = {};
-  vnp_Params['vnp_Version'] = '2.1.0';
-  vnp_Params['vnp_Command'] = 'pay';
-  vnp_Params['vnp_TmnCode'] = tmnCode;
-  // vnp_Params['vnp_Merchant'] = ''
-  vnp_Params['vnp_Locale'] = locale;
-  vnp_Params['vnp_CurrCode'] = currCode;
-  vnp_Params['vnp_TxnRef'] = orderId;
-  vnp_Params['vnp_OrderInfo'] = orderInfo;
-  vnp_Params['vnp_OrderType'] = orderType;
-  vnp_Params['vnp_Amount'] = amount * 100;
-  vnp_Params['vnp_ReturnUrl'] = returnUrl;
-  vnp_Params['vnp_IpAddr'] = ipAddr;
-  vnp_Params['vnp_CreateDate'] = createDate;
-  if (bankCode !== null && bankCode !== '') {
-    vnp_Params['vnp_BankCode'] = bankCode;
-  }
+//   var orderInfo = req.body.orderDescription;
+//   var orderType = req.body.orderType;
+//   var locale = req.body.language;
+//   if (locale === null || locale === '') {
+//     locale = 'vn';
+//   }
+//   var currCode = 'VND';
+//   var vnp_Params = {};
+//   vnp_Params['vnp_Version'] = '2.1.0';
+//   vnp_Params['vnp_Command'] = 'pay';
+//   vnp_Params['vnp_TmnCode'] = tmnCode;
+//   // vnp_Params['vnp_Merchant'] = ''
+//   vnp_Params['vnp_Locale'] = locale;
+//   vnp_Params['vnp_CurrCode'] = currCode;
+//   vnp_Params['vnp_TxnRef'] = orderId;
+//   vnp_Params['vnp_OrderInfo'] = orderInfo;
+//   vnp_Params['vnp_OrderType'] = orderType;
+//   vnp_Params['vnp_Amount'] = amount * 100;
+//   vnp_Params['vnp_ReturnUrl'] = returnUrl;
+//   vnp_Params['vnp_IpAddr'] = ipAddr;
+//   vnp_Params['vnp_CreateDate'] = createDate;
+//   if (bankCode !== null && bankCode !== '') {
+//     vnp_Params['vnp_BankCode'] = bankCode;
+//   }
 
-  vnp_Params = sortObject(vnp_Params);
+//   vnp_Params = sortObject(vnp_Params);
 
-  var querystring = require('qs');
-  var signData = querystring.stringify(vnp_Params, { encode: false });
-  var crypto = require("crypto");
-  var hmac = crypto.createHmac("sha512", secretKey);
-  var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");
-  vnp_Params['vnp_SecureHash'] = signed;
-  vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+//   var querystring = require('qs');
+//   var signData = querystring.stringify(vnp_Params, { encode: false });
+//   var crypto = require("crypto");
+//   var hmac = crypto.createHmac("sha512", secretKey);
+//   var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");
+//   vnp_Params['vnp_SecureHash'] = signed;
+//   vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
 
-  res.redirect(vnpUrl)
-});
+//   res.redirect(vnpUrl)
+// });
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'dghousepetshop115@gmail.com',
-    pass: 'vuem nuln cmxh bzqv',
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'dghousepetshop115@gmail.com',
+//     pass: 'vuem nuln cmxh bzqv',
+//   },
+// });
 
 const otpStorage = {};
 
