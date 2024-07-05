a68701d6a8fb596369e95bf25d80382573ba890d
long-troi-lo-dat <hnbaolong.4work@gmail.com>
2024-07-06 03:25:36 +0700

    test

--- front-end/src/pages/Cart.jsx
+++ front-end/src/pages/Cart.jsx
@@ -1,7 +1,6 @@
 import axios from "../axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import '../assets/css/global3.css';
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useContext } from "react";
