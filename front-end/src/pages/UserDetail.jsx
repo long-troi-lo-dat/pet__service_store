71c7cb205a6339b9f9b197ecbf0d69da87f4c728
long-troi-lo-dat <hnbaolong.4work@gmail.com>
2024-07-26 21:03:37 +0700

    update comment function + update user detail + update user detail update information

--- front-end/src/pages/UserDetail.jsx
+++ front-end/src/pages/UserDetail.jsx
@@ -6,21 +6,21 @@ function UserDetail() {
     const [user, setUser] = useState({});
     const { id } = useParams();
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const accessToken = localStorage.getItem('accessToken');
-                const response = await axios.get(`/api/auth/${id}`, {
-                    headers: { token: `Bearer ${accessToken}` },
-                });
-                setUser(response.data[0]);
-            } catch (error) {
-                console.log('Error fetching data:', error);
-            }
-        };
+    const fetchUserData = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            const response = await axios.get(`/api/auth/${id}`, {
+                headers: { token: `Bearer ${accessToken}` },
+            });
+            setUser(response.data[0]);
+        } catch (error) {
+            console.log('Error fetching data:', error);
+        }
+    };
 
+    useEffect(() => {
         fetchUserData();
-    }, [id]);
+    });
 
     return (
         <>
@@ -43,82 +43,52 @@ function UserDetail() {
                                 <div className="card">
                                     <div className="card-body">
                                         <div className="d-flex flex-column align-items-center text-center">
-                                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
+                                            {user.anhdaidien ? <img src={process.env.REACT_APP_URL_API + "/users/" + user.anhdaidien} alt="Admin" className="rounded-circle" width="150" /> : <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />}
                                             <div className="mt-3">
                                                 <h4>{user.hoTen}</h4>
-                                                {/* <p className="text-primary mb-1">Full Stack Developer</p> */}
-                                                {/* <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p> */}
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                {/* <div className="card mt-3">
-                                    <ul className="list-group list-group-flush">
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <p className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</p>
-                                            <span className="text-primary">https://bootdey.com</span>
-                                        </li>
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <p className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</p>
-                                            <span className="text-primary">bootdey</span>
-                                        </li>
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <p className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</p>
-                                            <span className="text-primary">@bootdey</span>
-                                        </li>
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <p className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</p>
-                                            <span className="text-primary">bootdey</span>
-                                        </li>
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <p className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</p>
-                                            <span className="text-primary">bootdey</span>
-                                        </li>
-                                    </ul>
-                                </div> */}
                             </div>
                             <div className="col-md-8">
                                 <div className="card mb-3">
                                     <div className="card-body">
-                                        <div className="row">
+                                        <div className="row pb-2">
                                             <div className="col-sm-3">
                                                 <p className="">Họ tên</p>
                                             </div>
-                                            <div className="col-sm-9 text-primary">
-                                                {user.hoTen ? <p>{user.hoTen}</p> : <p className="text-dark">Chưa có họ và tên</p>}
+                                            <div className="col-sm-9 text-dark">
+                                                {user.hoTen ? <p style={{ textTransform: "capitalize" }}>{user.hoTen}</p> : <p className="text-dark">Chưa có họ và tên</p>}
                                             </div>
                                         </div>
-                                        <hr />
-                                        <div className="row">
+                                        <div className="row pb-2">
                                             <div className="col-sm-3">
                                                 <p className="">Email</p>
                                             </div>
-                                            <div className="col-sm-9 text-primary">
+                                            <div className="col-sm-9 text-dark">
                                                 {user.email ? <p>{user.email}</p> : <p className="text-dark">Chưa có email</p>}
                                             </div>
                                         </div>
-                                        <hr />
-                                        <div className="row">
+                                        <div className="row pb-2">
                                             <div className="col-sm-3">
                                                 <p className="">Số điện thoại</p>
                                             </div>
-                                            <div className="col-sm-9 text-primary">
+                                            <div className="col-sm-9 text-dark">
                                                 {user.sdt ? <p>{user.sdt}</p> : <p className="text-dark">Chưa có số điện thoại</p>}
                                             </div>
                                         </div>
-                                        <hr />
-                                        <div className="row">
+                                        <div className="row pb-2">
                                             <div className="col-sm-3">
                                                 <p className="">Địa chỉ</p>
                                             </div>
-                                            <div className="col-sm-9 text-primary">
-                                                {user.diachi ? <p>{user.diachi}</p> : <p className="text-dark">Chưa có địa chỉ</p>}
+                                            <div className="col-sm-9 text-dark">
+                                                {user.diachi ? <p style={{ textTransform: "capitalize" }}>{user.diachi}</p> : <p className="text-dark">Chưa có địa chỉ</p>}
                                             </div>
                                         </div>
-                                        <hr />
-                                        <div className="row">
+                                        <div className="row pb-2">
                                             <div className="col-sm-12">
-                                                <Link to="" className="btn btn-dark btn-md rounded-1">Sửa thông tin</Link>
+                                                <Link to={"/userdetailedit/" + id} className="btn btn-dark btn-md rounded-1">Sửa thông tin</Link>
                                             </div>
                                         </div>
                                     </div>
--- front-end/src/pages/UserDetailEdit.jsx
+++ front-end/src/pages/UserDetailEdit.jsx
@@ -2,69 +2,98 @@ import React, { useEffect, useState } from "react";
 import axios from '../axios';
 import { useNavigate, useParams } from "react-router-dom";
 
-function UserDetailEdit(props) {
-
+function UserDetailEdit() {
     const navigate = useNavigate();
     const { id } = useParams();
 
-    const [editUser, setEditUser] = useState({ hoTen: "", anhdaidien: "", sdt: "", diachi: "" });
-    const handleInput = (e) => {
-        setEditUser({ ...editUser, [e.target.name]: e.target.value });
-    }
-    const handleUpdate = async (e) => {
-        const editInputvalue = { hoTen: editUser.hoTen, anhdaidien: editUser.anhdaidien, sdt: editUser.sdt, diachi: editUser.diachi, id_user: localStorage.getItem("id_user") };
-        await axios
-            .post("/updatethongtin", editInputvalue)
-            .then((res) => {
-                setTimeout(() => {
-                    navigate(`/userdetail/${localStorage.getItem("id_user")}`);
-                }, 1000);
-            })
-            .catch((err) => console.log(err));
-    }
+    const [editUser, setEditUser] = useState({
+        hoTen: "",
+        sdt: "",
+        diachi: "",
+        matkhau: "",
+    });
+    const [image, setImage] = useState(null);
+
+    const handleChangeInput = (event) => {
+        const { name, value } = event.target;
+        setEditUser({ ...editUser, [name]: value });
+    };
+
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+    };
+
+    const handleUpdate = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('hoTen', editUser.hoTen);
+            formData.append('sdt', editUser.sdt);
+            formData.append('diachi', editUser.diachi);
+            formData.append('matkhau', editUser.matkhau);
+            if (image) {
+                formData.append('anhdaidien', image);
+            }
+            formData.append('id_user', id);
+
+            await axios.put(`/api/auth/${id}`, formData);
+            navigate(`/userdetail/${id}`);
+        } catch (err) {
+            console.error('Error updating user details:', err);
+        }
+    };
 
     useEffect(() => {
-        axios.get(`/userdetail/${id}`)
-            .then((response) => {
-                setEditUser(response.data[0]);
+        axios.get(`/api/auth/${id}`)
+            .then(response => {
+                setEditUser(prevState => ({
+                    ...prevState,
+                    ...response.data[0]
+                }));
             })
-            .catch((error) => {
-                console.error('error fetching data :', error);
+            .catch(error => {
+                console.error('Error fetching data:', error);
             });
     }, [id]);
+
     return (
-        <>
-            <div className="container rounded bg-white mt-5 mb-5">
-                <div className="row">
-                    <div className="col-md-12 border">
-                        <div className="p-3 py-5">
-                            <div className="d-flex justify-content-between align-items-center mb-3">
-                                <h4 className="text-right">Thay đổi thông tin cá nhân</h4>
+        <div className="container rounded bg-white mt-5 mb-5">
+            <div className="row">
+                <div className="col-md-12 border">
+                    <div className="p-3 py-5">
+                        <div className="d-flex justify-content-between align-items-center mb-3">
+                            <h4 className="text-right">Thay đổi thông tin cá nhân</h4>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <label className="labels">Họ và tên</label>
+                                <input type="text" className="form-control" name="hoTen" style={{ textTransform: "capitalize" }} value={editUser.hoTen} onChange={handleChangeInput} />
                             </div>
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <label className="labels">Họ và tên</label><div style={{ display: "flex", lineHeight: "38px" }}><input type="text" className="form-control" name="hoTen" value={editUser.hoTen} onChange={handleInput} /></div>
-                                </div>
-                                <div className="col-md-12 mt-3">
-                                    <label className="labels">Ảnh đại diện</label> <div style={{ display: "flex", lineHeight: "38px" }}><input type="text" className="form-control" name="anhdaidien" value={editUser.anhdaidien} onChange={handleInput} /></div>
-                                </div>
-                                <div className="col-md-12 mt-3">
-                                    <label className="labels">Số điện thoại</label> <div style={{ display: "flex", lineHeight: "38px" }}><input type="text" className="form-control" name="sdt" value={editUser.sdt} onChange={handleInput} /></div>
-                                </div>
-                                <div className="col-md-12 mt-3">
-                                    <label className="labels">Địa chỉ</label><div style={{ display: "flex", lineHeight: "38px" }}><input type="text" className="form-control" name="diachi" value={editUser.diachi} onChange={handleInput} /></div>
-                                </div>
-                                <div className="col-md-12 mt-3 d-flex justify-content-center">
-                                    <button onClick={() => navigate(`/userdetail/${localStorage.getItem("id_user")}`)} className="btn btn-danger" style={{ minWidth: "200px" }}>Hủy cập nhật</button>
-                                    <button style={{ margin: "0 15px" }}></button>
-                                    <button onClick={() => handleUpdate()} name="submit" className="btn btn-success" style={{ minWidth: "200px" }}>Cập nhật thông tin</button>
-                                </div>
+                            <div className="col-md-12 mt-3">
+                                <label className="labels">Ảnh đại diện</label>
+                                <input type="file" className="form-control" name="anhdaidien" onChange={handleImageChange} />
+                            </div>
+                            <div className="col-md-12 mt-3">
+                                <label className="labels">Số điện thoại</label>
+                                <input type="text" className="form-control" name="sdt" value={editUser.sdt} onChange={handleChangeInput} />
+                            </div>
+                            <div className="col-md-12 mt-3">
+                                <label className="labels">Mật khẩu</label>
+                                <input type="password" className="form-control" name="matkhau" value={editUser.matkhau} onChange={handleChangeInput} />
+                            </div>
+                            <div className="col-md-12 mt-3">
+                                <label className="labels">Địa chỉ</label>
+                                <input type="text" className="form-control" name="diachi" value={editUser.diachi} onChange={handleChangeInput} />
+                            </div>
+                            <div className="col-md-12 mt-3 d-flex justify-content-center">
+                                <button onClick={() => navigate(`/userdetail/${localStorage.getItem("id_user")}`)} className="btn btn-danger" style={{ minWidth: "200px" }}>Hủy cập nhật</button>
+                                <div style={{ margin: "0 15px" }}></div>
+                                <button onClick={handleUpdate} className="btn btn-success" style={{ minWidth: "200px" }}>Cập nhật thông tin</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
