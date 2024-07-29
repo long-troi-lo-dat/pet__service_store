60cc0c47c3814a550f95ec763365042a93f67ba3
long-troi-lo-dat <hnbaolong.4work@gmail.com>
2024-07-29 14:53:01 +0700

    fix upload image user

--- back-end/src/config/multer.js
+++ back-end/src/config/multer.js
@@ -21,7 +21,7 @@ const commentImageStorage = multer.diskStorage({
 
 const userImageStorage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + "../../uploads/users");
+        cb(null, path.join(process.cwd(), "/uploads/users"));
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}_${file.originalname}`);
